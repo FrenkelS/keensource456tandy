333bb1d2d886794ea8d71e4f79ec0b6dea19819a
Frenkel Smeijers <frenkel.smeijers@rabobank.nl>
2022-01-03 12:56:40 +0100

    move code from header to c file that is only used in ID_MM.C

--- KEEN4-6/ID_MM.C
+++ KEEN4-6/ID_MM.C
@@ -62,6 +62,11 @@ EMS / XMS unmanaged routines
 =============================================================================
 */
 
+#define SAVENEARHEAP	0x400		// space to leave in data segment
+#define SAVEFARHEAP		0			// space to leave in far heap
+
+#define MAXBLOCKS		1200
+
 #define LOCKBIT		0x80	// if set in attributes, block cannot be moved
 #define PURGEBIT	1		// 0= unpurgeable, 1= purge
 #define BASEATTRIBUTES	0	// unlocked, non purgeable
@@ -83,6 +88,44 @@ typedef struct mmblockstruct
 #define GETNEWBLOCK {if(!mmfree)MML_ClearBlock();mmnew=mmfree;mmfree=mmfree->next;}
 
 #define FREEBLOCK(x) {*x->useptr=NULL;x->next=mmfree;mmfree=x;}
+
+
+//--------
+
+#define	EMS_INT			0x67
+
+#define	EMS_STATUS		0x40
+#define	EMS_GETFRAME	0x41
+#define	EMS_GETPAGES	0x42
+#define	EMS_ALLOCPAGES	0x43
+#define	EMS_MAPPAGE		0x44
+#define	EMS_FREEPAGES	0x45
+#define	EMS_VERSION		0x46
+
+//--------
+
+#define	XMS_VERSION		0x00
+
+#define	XMS_ALLOCHMA	0x01
+#define	XMS_FREEHMA		0x02
+
+#define	XMS_GENABLEA20	0x03
+#define	XMS_GDISABLEA20	0x04
+#define	XMS_LENABLEA20	0x05
+#define	XMS_LDISABLEA20	0x06
+#define	XMS_QUERYA20	0x07
+
+#define	XMS_QUERYREE	0x08
+#define	XMS_ALLOC		0x09
+#define	XMS_FREE		0x0A
+#define	XMS_MOVE		0x0B
+#define	XMS_LOCK		0x0C
+#define	XMS_UNLOCK		0x0D
+#define	XMS_GETINFO		0x0E
+#define	XMS_RESIZE		0x0F
+
+#define	XMS_ALLOCUMB	0x10
+#define	XMS_FREEUMB		0x11
 
 /*
 =============================================================================
